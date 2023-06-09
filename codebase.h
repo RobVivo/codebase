9c6d3b8f65a9498dcf373edd90eb8069f808f63c
R.Vivó <rvivo@upv.es>
2023-06-09 23:30:12 +0200

    fixes

--- codebase.h
+++ codebase.h
@@ -13,8 +13,9 @@
 #include <iostream>
 #include <cmath>
 #include <array>
-#include <GL/glext.h>
+
 #include <GL/freeglut.h>
+#include <GL/glext.h>
 #include <freeimage/FreeImage.h>
 
 using namespace std;
@@ -59,7 +60,7 @@ public:
 	Vec3 operator/(float f) const { return Vec3(x / f, y / f, z / f); }
 	Vec3& operator+=(const Vec3& v) { x += v.x; y += v.y; z += v.z; return *this; }
 	Vec3& normalize() { return *this = *this * (1.0f / sqrtf(x * x + y * y + z * z)); }
-    float norm() const { return sqrtf(x*x+y*y+z*z) }
+	float norm() const { return sqrtf(x * x + y * y + z * z); }
     float dot(const Vec3& other) const { return x * other.x + y * other.y + z * other.z; }
 	Vec3 cross(const Vec3& other) const {return Vec3(y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x); }
 	Vec3& rotX(float a) { float c = cosf(a), s = sinf(a), y0 = y; y = y0 * c - z * s; z = y0 * s + z * c; return *this; }
@@ -68,7 +69,7 @@ public:
 	Vec3& rotate(float a, const Vec3& axis) {
         Vec3 u = axis / axis.norm();
         // Formula de Rodrigues
-        *this = *this * cosf(alfa) + u.cross(*this) * sinf(alfa) + u * u.dot(*this) * (1 - cosf(alfa));
+        *this = *this * cosf(a) + u.cross(*this) * sinf(a) + u * u.dot(*this) * (1 - cosf(a));
         return *this;
     }
 	array<float, 3> toArray() const { return { x,y,z }; }
